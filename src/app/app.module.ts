af0df1ee1678804197a90c182ed3dff8ee8bcc98
Matt Payne <Payne@MattPayne.org>
2018-08-05 18:08:15 -0500

    mat-slider is working! :-)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatSlider} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatSliderModule} from '@angular/material/slider';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule,MatButtonModule, MatCheckboxModule, MatSlider,
+    BrowserModule, BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
