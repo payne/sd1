ccd08741afc08aec2f321822d11cdd10694c6442
Matt Payne <Payne@MattPayne.org>
2018-08-05 18:25:33 -0500

    slider value change event working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import {coerceNumberProperty} from '@angular/cdk/coercion';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = true;
+
+  onChange(event) {
+    this.value = event.value;
+  }
+  
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(value) {
+    this._tickInterval = coerceNumberProperty(value);
+  }
+  private _tickInterval = 1;
+
 }
