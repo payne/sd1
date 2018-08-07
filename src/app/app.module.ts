70424c4ca94dea7f7ea2036dbcd0867335c35448
Matt Payne <Payne@MattPayne.org>
2018-08-07 18:04:56 -0500

    MatTabsModule added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule,MatTabsModule} from '@angular/material';
 import {MatSliderModule} from '@angular/material/slider';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,MatSliderModule,
+    BrowserModule, BrowserAnimationsModule,MatButtonModule, MatCheckboxModule,
+    MatTabsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
